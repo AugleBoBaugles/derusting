c8e927c23d3ee1cb40c93b96ffac06464d999b3a
Augy Markham <katiefaithmarkham@gmail.com>
2025-01-06 22:13:05 -0800

    completed MapPractice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,29 +1,58 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    Map<String, Integer> myMap = new HashMap<String, Integer>();
+    System.out.println(myMap);
+
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
+    myMap.put("i", 56);
+    myMap.put("me", 23);
+    myMap.put("mine", 99);
+    System.out.println(myMap);
 
     // Get the value associated with a given key in the Map
+    System.out.println(myMap.get("me"));
 
     // Find the size (number of key/value pairs) of the Map
+    System.out.println("size: " + myMap.size());
 
     // Replace the value associated with a given key (the size of the Map shoukld not change)
+    myMap.replace("mine", 2);
+    System.out.println(myMap);
 
     // Check whether the Map contains a given key
+    System.out.println("Contains \"you\"? " + myMap.containsKey("you"));
+    System.out.println("Contains \"me\"? " + myMap.containsKey("me"));
 
     // Check whether the Map contains a given value
+    System.out.println("Contains \"1\"? " + myMap.containsValue(1));
+    System.out.println("Contains \"2\"? " + myMap.containsValue(2));
 
     // Iterate over the keys of the Map, printing each key
+    System.out.println("--- Keys ---");
+    for (String key : myMap.keySet()){
+      System.out.println(key);
+    }
+    System.out.println();
 
     // Iterate over the values of the map, printing each value
+    System.out.println("--- Values ---");
+    for (int val : myMap.values()){
+      System.out.println(val);
+    }
+    System.out.println();
 
     // Iterate over the entries in the map, printing each key and value
-
+    System.out.println("--- Sets ---");
+    for (String key : myMap.keySet()){
+      System.out.println(key + " - " + myMap.get(key));
+    }
     /*
      * Usage tip!
      * 
